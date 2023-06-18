450aa392bb059534c8024df27d3dc2ad34cd705f
LukasThome <thome_lucas@live.com>
2023-06-18 18:54:36 -0300

    prototip front

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-
-import { router } from "./routes";
-import { RouterProvider } from "react-router-dom";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
