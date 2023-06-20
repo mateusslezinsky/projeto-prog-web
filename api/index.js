cfcf41c4f337be48fdc5e855bd897b17d8d90eca
LukasThome <thome_lucas@live.com>
2023-06-19 23:56:41 -0300

    cors adicionado

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+
+app.post('/register', (req, res) => {
+    res.json('test ok')
+})
+
+
+app.listen(4000);
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,9 +1,27 @@
-export default function RegisterPage(){
+import { useState } from "react";
+
+export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    async function register(ev) {
+        ev.preventDefault();
+        await fetch('http://localhost:4000', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+    }
     return (
-        <form className="register">
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+            <input type="text"
+                placeholder="username"
+                value={username}
+                onChange={ev => setUsername(ev.target.value)} />
+            <input type="password"
+                placeholder="password"
+                value={password}
+                onChange={ev => setPassword(ev.target.value)} />
             <button>Register</button>
 
         </form>
