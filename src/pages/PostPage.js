a819618e675d554fc2047280c73da618eb7a07cf
LukasThome <thome_lucas@live.com>
2023-06-23 09:06:38 -0300

    fixes

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,43 +1,64 @@
-import {useContext, useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import {formatISO9075} from "date-fns";
-import {UserContext} from "../UserContext";
-import {Link} from 'react-router-dom';
+import {useEffect, useState} from "react";
+import {Navigate, useParams} from "react-router-dom";
+import Editor from "./Editor"
 
-export default function PostPage() {
-  const [postInfo,setPostInfo] = useState(null);
-  const {userInfo} = useContext(UserContext);
+export default function EditPost() {
   const {id} = useParams();
+  const [title,setTitle] = useState('');
+  const [summary,setSummary] = useState('');
+  const [content,setContent] = useState('');
+  const [files, setFiles] = useState('');
+  const [redirect,setRedirect] = useState(false);
+
   useEffect(() => {
-    fetch(`http://localhost:4000/post/${id}`)
+    fetch('http://localhost:4000/post/'+id)
       .then(response => {
         response.json().then(postInfo => {
-          setPostInfo(postInfo);
+          setTitle(postInfo.title);
+          setContent(postInfo.content);
+          setSummary(postInfo.summary);
         });
       });
   }, []);
 
-  if (!postInfo) return '';
+  async function updatePost(ev) {
+    ev.preventDefault();
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('id', id);
+    if (files?.[0]) {
+      data.set('file', files?.[0]);
+    }
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'PUT',
+      body: data,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/post/'+id} />
+  }
 
   return (
-    <div className="post-page">
-      <h1>{postInfo.title}</h1>
-      <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
-      <div className="author">by @{postInfo.author.username}</div>
-      {userInfo.id === postInfo.author._id && (
-        <div className="edit-row">
-          <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-            </svg>
-            Edit this post
-          </Link>
-        </div>
-      )}
-      <div className="image">
-        <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
-      </div>
-      <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
-    </div>
+    <form onSubmit={updatePost}>
+      <input type="title"
+             placeholder={'Title'}
+             value={title}
+             onChange={ev => setTitle(ev.target.value)} />
+      <input type="summary"
+             placeholder={'Summary'}
+             value={summary}
+             onChange={ev => setSummary(ev.target.value)} />
+      <input type="file"
+             onChange={ev => setFiles(ev.target.files)} />
+      <Editor onChange={setContent} value={content} />
+      <button style={{marginTop:'5px'}}>Update post</button>
+    </form>
   );
 }
