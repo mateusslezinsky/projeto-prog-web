412414d5b3070f6608566baab8f43a1f54670873
augustotsilva <augusto.silva@netpos.com.br>
2023-06-26 16:02:14 -0300

    augusto
    # small tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,49 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { UserContext } from "../UserContext";
+import {Link, useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import {UserContext} from "../UserContext";
 
-import { auth } from "../firebase/firebase.utils";
-import { signOut } from "firebase/auth";
+import {auth} from "../firebase/firebase.utils";
+import {signOut} from "firebase/auth";
 
 export default function Header() {
-  const { userInfo, setUserInfo } = useContext(UserContext);
+    const {userInfo, setUserInfo} = useContext(UserContext);
 
-  const navigate = useNavigate();
-  const onLogout = async () => {
-    try {
-      await signOut(auth);
-      setUserInfo(null);
-      navigate("/");
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    const navigate = useNavigate();
+    const onLogout = async () => {
+        try {
+            await signOut(auth);
+            setUserInfo(null);
+            navigate("/");
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-  return (
-    <header>
-      <Link to="/" className="logo">
-        Auluma News
-      </Link>
+    return (
+        <header>
+            <Link to="/" className="logo">
+                Auluma News
+            </Link>
 
-      {userInfo ? (
-        <>
-          <div>Olá, {userInfo.name}</div>
-          <nav>
-            <Link to="/create">Criar novo post</Link>
-            <Link to="/user">Minha conta</Link>
-            <button className="logout" onClick={onLogout}>
-              Logout
-            </button>
-          </nav>
-        </>
-      ) : (
-        <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Registrar</Link>
-        </nav>
-      )}
-    </header>
-  );
+            {userInfo ? (
+                <>
+                    <div>
+                        <nav>
+                            <div className={"center hidden-navbar-item"}>Olá, <b>{userInfo.name}</b></div>
+                            <Link className={"hidden-navbar-item"} to="/create">Criar novo post</Link>
+                            <Link className={"hidden-navbar-item"} to="/user">Minha conta</Link>
+                            <button className="logout" onClick={onLogout}>
+                                Logout
+                            </button>
+                        </nav>
+                    </div>
+                </>
+            ) : (
+                <nav>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Registrar</Link>
+                </nav>
+            )}
+        </header>
+    );
 }
